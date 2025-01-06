ff46523ffdfee4d9ea6874fa22b5980e98cf187a
winter-sand <brayden.remington@thekaustschool.org>
2025-01-06 21:58:28 +0300

    Progess
    trying to get the math to work out for the thing as a whole

--- attempt-6-i-think/script.gd
+++ attempt-6-i-think/script.gd
@@ -1,16 +1,51 @@
 extends Node2D
 
+func load_data() -> Dictionary:
+	var t10k_images_idx3_ubyte = FileAccess.open("res://MNIST_Data/t10k-images-idx3-ubyte", FileAccess.READ)
+	var t10k_labels_idx1_ubyte = FileAccess.open("res://MNIST_Data/t10k-labels-idx1-ubyte", FileAccess.READ)
+	var train_images_idx3_ubyte = FileAccess.open("res://MNIST_Data/train-images-idx3-ubyte", FileAccess.READ)
+	var train_labels_idx1_ubyte = FileAccess.open("res://MNIST_Data/train-labels-idx1-ubyte", FileAccess.READ)
+	
+	
+	var test_images = t10k_images_idx3_ubyte.get_buffer(t10k_images_idx3_ubyte.get_length()).slice(16)
+	var test_labels = t10k_labels_idx1_ubyte.get_buffer(t10k_labels_idx1_ubyte.get_length()).slice(8)
+	var train_images = train_images_idx3_ubyte.get_buffer(train_images_idx3_ubyte.get_length()).slice(16)
+	var train_labels = train_labels_idx1_ubyte.get_buffer(train_labels_idx1_ubyte.get_length()).slice(8)
+	
+	#print(len(train_labels))
+	#print(len(train_images))
+	
+	t10k_images_idx3_ubyte.close()
+	t10k_labels_idx1_ubyte.close()
+	train_images_idx3_ubyte.close()
+	train_labels_idx1_ubyte.close()
+	
+	return {
+		"test_images" : test_images,
+		"test_labels" : test_labels,
+		"train_images" : train_images,
+		"train_labels" : train_labels,
+	}
+var data = load_data()
+
+const e = 2.718281828459045
 var rng = RandomNumberGenerator.new()
-var pos_in_dimensions: Array = []
-var ideal_pos: Array = []
-var LR: float = 0.03
+#var pos_in_dimensions: Array = []
+#var ideal_pos: Array = []
+const LR: float = 0.03
 @onready var cost: float = 0.0
 
-func calculate_cost(pos: Array, ideal: Array) -> float:
-	var cost: float = 0.0
-	for i in range(len(pos)):
-		cost += (pos[i]-ideal[i])**2  # 1 is the ideal value for all the vars to set to, i can change it if needed on a per-var basis
-	return cost
+var images = data["test_images"]
+var labels = data["test_labels"]
+var input_size: int = 784
+var input: Array = empty_array(784)
+
+var layers: Array = [16, 16, 10]
+var a: Array = []
+var w: Array = []
+var b: Array = []
+var z: Array = []
+var y: Array = []
 
 func per_axis_derivate(pos: Array, ideal: Array) -> Array:  # pd = partial derivative. I would use ∂ but it doesn't let me
 	var pds: Array = []
@@ -18,16 +53,106 @@ func per_axis_derivate(pos: Array, ideal: Array) -> Array:  # pd = partial deriv
 		pds.append(2*(pos[i]-ideal[i]))  # derivative of the cost function (the other sections of adding each of the other costants for the other demensions don't matter becuse derivation puts constants at 0
 	return pds
 
+func initialize():
+	var previous_layer_length: float = input_size  # float because needed for other calcs. I could do the (float) operation (evaluate as float) but this is much easier
+	for L in range(len(layers)):
+		a.append([])
+		w.append([])
+		b.append([])
+		z.append([])
+		y.append([])
+		for j in range(layers[L]):
+			a[L].append(0.0)
+			w[L].append([])
+			b[L].append(0.0)
+			z[L].append(0.0)
+			y[L].append(0.0)
+			for k in range(previous_layer_length):
+				w[L][j].append(rng.randf_range(-sqrt(1/previous_layer_length), sqrt(1/previous_layer_length)))
+		#print(previous_layer_length)
+		previous_layer_length = layers[L]
+
+func σ(x):
+	return 1/(1+e**(-x*0.3))
+
+func dσ(x):
+	var sig = σ(x)
+	return sig*(1-sig)
+
+func sum(list: Array):
+	var out = 0
+	for i in range(len(list)):
+		out += list[i]
+	return out
+
+func map(x:float, xmin:float, xmax:float, min:float, max:float):
+	return (max-min)*((x-xmin)/(xmax-xmin))+min
+
+func empty_array(size: int) -> Array:
+	var arr: Array = []
+	for i in range(size):
+		arr.append(0.0)
+	return arr
+
+func set_input(imageN: int = 0, refined = true):
+	input = images.slice(imageN*input_size, (imageN+1)*input_size)
+	if !refined:
+		for pixel in range(len(input)):
+			input[pixel] = map(input[pixel], 0, 255, 0, 1)
+
+func foreprop():
+	var previous_layer: Array = input
+	for L in range(len(layers)):
+		for j in range(layers[L]):
+			z[L][j] = b[L][j]
+			for k in range(len(previous_layer)):
+				z[L][j] += previous_layer[k]*w[L][j][k]
+			a[L][j] = σ(z[L][j])
+		previous_layer = a[L]
+
+func calc_cost(ideal):
+	var final: float
+	if typeof(ideal) == 2:  # 2 = int variable type id
+		for i in range(len(y[-1])): y[-1][i] = 0
+		y[-1][ideal] = 1
+	for i in range(len(a[-1])):
+		final += (a[-1][i]-y[-1][i])**2
+	return final
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	for i in range(13002): pos_in_dimensions.append(rng.randf_range(-30, 30)); ideal_pos.append(i);
-	
+	initialize()
+	#print(len(w[0][0]))
+	#print(len(w[0]))
+	#print(a)
+	#print(z)
+	#print(sum(input))
+	#print(w)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+var reps:int = 0
 func _process(delta: float) -> void:
-	cost = calculate_cost(pos_in_dimensions, ideal_pos)
-	print(cost)
-	var pds = per_axis_derivate(pos_in_dimensions, ideal_pos)
-	for i in range(len(pos_in_dimensions)):
-		pos_in_dimensions[i] -= pds[i]*LR
-	#print(pos_in_dimensions)
+	#print(reps)
+	# foreprop
+	#set_input(0, false)  # constant image set cuz I just want to see if the math works, not if it can recognize
+	foreprop()
+	# get/print cost
+	print(z)
+	print()
+	print(a)
+	print()
+	print(calc_cost(labels[0]))
+	print()
+	print()
+	print()
+	# get derrivative of last layers inputs (y for the last layer (but moving backwards) is the final ideal, and for previous layers it changes)
+		# get ∂ to each weight (a[L-1][k] * dσ(z[L][j]) * 2(a[L][j] - y[L][j]))
+		# bias (1 * dσ(z[L][j]) * 2(a[L][j] - y[L][j]))
+		# previous neuron (a[L-1][j]) ∑(k): (w[L][j][k] * dσ(z[L][j]) * 2(a[L][j] - y[L][j]))
+			# set y(ideal) for previous layers to current value - previously established derrivitive * LR: y[L][j] = a[L][j]-(∑(k): (w[L][j][k] * dσ(z[L][j]) * 2(a[L][j] - y[L][j])))*LR
+	# minus each weight by it's derrivitive times the LR: w[L][j][k] -= (∂w[L][j][k])*LR:		((a[L-1][k] * dσ(z[L][j]) * 2(a[L][j] - y[L][j]))*LR)
+	# bias: b[L][j] -= (∂b[L][j])*LR:		(1 * dσ(z[L][j]) * 2(a[L][j] - y[L][j]))
+	
+	"""This will train as 1 image at a time, eventualy I will want to get the average modifications per weight/bias and use that for the changes. this works for now"""
+	
+	reps += 1
